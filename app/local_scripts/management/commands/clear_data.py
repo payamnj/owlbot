87f2f34a66a95c9be7826dd55ba590f04f6b6a88
Payam Najafizadeh <payamnajafizadeh@Payams-MacBook-Air.local>
2017-04-30 11:40:32 +0100

    unpublish wrong defenitions

--- app/local_scripts/management/commands/clear_data.py
+++ app/local_scripts/management/commands/clear_data.py
@@ -7,6 +7,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         Defenition = apps.get_model('dictionary', 'Defenition')
         defenitions = Defenition.objects.all()
+
+        unpublish_list = [
+            'Australian/NZ', 'Physiology', 'historical', 'Billiards',
+            'Military', 'Nautical', 'Anatomy', 'dialect', 'Greek Mythology',
+            'see', 'Photography', 'Biology', 'Soccer', 'North American',
+            'Astrology', 'formal', 'Entomology', 'Christian Theology',
+            'Stock Exchange', 'Theology',  'American Football', 'urinate.',
+            'Biochemistry', 'Medicine', 'Rugby', 'derogatory', 'dated',
+            'Zoology', 'Genetics', 'Geology', 'Physics', 'Phonetics', 'vomit.',
+            'vulgar slang', 'Law', 'Cricket', 'nonsense.', 'Baseball',
+            'South African', 'Philosophy', 'Grammar', 'Scottish', 'Astronomy',
+            'Architecture', 'Linguistics', 'Bridge', 'informal', 'Computing',
+            'Logic', 'archaic', 'Irish', 'West Indian', 'literary', 'Canadian',
+            'technical', 'Heraldry', 'Sailing', 'Geometry', 'Australian',
+            'British', 'Statistics', 'Mathematics', 'Printing', 'Botany',
+            'Music', 'trademark', 'steal.', 'Christian Church', 'a lesbian.',
+            'Chemistry', 'rare', 'US', 'Finance', 'Golf', 'Electronics']
+
         for defenition in defenitions:
 
             if defenition.example:
@@ -15,3 +33,11 @@ class Command(BaseCommand):
                     print(example)
                     defenition.example = example[1:-1]
                     defenition.save()
+
+            
+            if defenition.defenition in unpublish_list:
+                defenition.published = False
+                defenition.save()
+
+            
+
