04defead22c52902c7b0ea7a63ffa8a39318564b
Payam Najafizadeh <payamnajafizadeh@Payams-MacBook-Air.local>
2017-05-08 23:29:23 +0100

    add gcp settings

---
+++ app/core/settings/gcp.py
@@ -0,0 +1,23 @@
+from .settings import *
+from .utils import get_env_variable
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+THUMBNAIL_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['www.owlbot.info', 'owlbot.info', 'gcp.owlbot.info']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DB_NAME'),
+        'USER': get_env_variable('DB_USER'),
+        'PASSWORD': get_env_variable('DB_PASSWORD'),
+        'HOST': get_env_variable('DB_HOST'),
+        'PORT': '',
+    },
+}
