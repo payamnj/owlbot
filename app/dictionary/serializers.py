3a8af1f0f022663074dd6519298772af7256d824
Payam <payam.nj@gmail.com>
2017-05-24 18:50:12 +0100

    Update serializers.py

--- app/dictionary/serializers.py
+++ app/dictionary/serializers.py
@@ -4,12 +4,7 @@ import models
 
 
 class DefenitionSerializer(serializers.ModelSerializer):
-    
-    test_field = serializers.SerializerMethodField()
-    
-    def get_test_field(self, obj):
-        return 'test'
 
     class Meta:
         model = models.Defenition
-        fields = ('type', 'defenition', 'example', 'test_field')
+        fields = ('type', 'defenition', 'example')
