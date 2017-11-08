ada3da15a13df1e16a7910f237596c0bbe1b8eeb
Payam <payam.nj@gmail.com>
2017-11-08 22:00:57 +0100

    Update serializers.py

--- app/dictionary/serializers.py
+++ app/dictionary/serializers.py
@@ -8,3 +8,15 @@ class DefenitionSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Defenition
         fields = ('type', 'defenition', 'example')
+        
+
+class DefinitionSerializer(serializers.ModelSerializer):
+
+    definition = serializers.SerializerMethodField()
+    
+    def get_definition(self, obj):
+        return obj.defenition
+
+    class Meta:
+        model = models.Defenition
+        fields = ('type', 'definition', 'example')
