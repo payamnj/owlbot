e87574a07a9bf08a469d65a0839089a06d55c27f
Payam <payam.nj@gmail.com>
2019-02-17 13:26:21 +0100

    get secret key from evnironment variables

--- app/core/settings/settings.py
+++ app/core/settings/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$o5m(eq4zgj)_@hc#p^47k=o9fcmb(k_ykuy&y$(*wr1=83i82'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
