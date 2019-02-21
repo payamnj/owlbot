d4d79dac6d76c63db613f2fd98164cc57d775508
Payam <payam.nj@gmail.com>
2019-02-21 20:44:27 +0100

    random word

--- app/dictionary/views.py
+++ app/dictionary/views.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic.base import RedirectView
 from app.dictionary import models
 from app.dictionary import serializers
+import random
 import requests
 
 
@@ -41,7 +42,9 @@ class GA(object):
 
 
 class Home(RedirectView):
-    url = '/api/v2/dictionary/owl'
+    rand_num = random.randint(0, 100744)
+    rand_word = models.Word.objects.all()[rand_num:1]
+    url = '/api/v2/dictionary/{}'.format(rand_word.word)
 
 
 class DefinitionApi(APIView, GA):
