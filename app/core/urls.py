6ef209ab4cd6eef08e10f00e2cc22e3e37042a4c
Payam <payam.nj@gmail.com>
2019-03-02 19:27:12 +0100

    temporary verification url

--- app/core/urls.py
+++ app/core/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from app.dictionary import views as dictionary_views
 from django.urls import path
+from django.views.generic import TemplateView
 
 
 api_v1_urlpatterns = [
@@ -14,6 +15,7 @@ api_v2_urlpatterns = api_v1_urlpatterns
 
 urlpatterns = [
     path('joghdadmin/', admin.site.urls),
+    path('google582736430f609802.html', TemplateView.as_view(template_name='google582736430f609802.html')),
     url(r'^$', dictionary_views.Home.as_view()),
     url(r'^api/v1/', include(api_v1_urlpatterns)),
     url(r'^api/v2/', include(api_v2_urlpatterns))
